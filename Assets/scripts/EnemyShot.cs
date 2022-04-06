6276a2ce25da18d2930ce77e22b0664d1c03d61b
Juan Fernández Temprano <fertemjua@IESPIRINGALLA.AD>
2022-04-06 20:48:19 +0200

    EnemyShot creado #2

--- Assets/scripts/EnemyShot.cs
+++ Assets/scripts/EnemyShot.cs
@@ -16,6 +16,13 @@ public class EnemyShot : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //Probabilidad de 0 entre 1.000
+        if(Random.value <= 0.001f){
+            Shot();
+        }
+    }
+
+    private void Shot(){
+
     }
 }
